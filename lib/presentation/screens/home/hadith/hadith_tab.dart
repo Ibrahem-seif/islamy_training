85050e93b85ed98c93b4518906fda5fd049b713a
Ibrahem-seif <ibrahemsaeed118@gmail.com>
2024-10-19 19:08:05 +0300

    % hadith_tab ui + hadith_tab logic

--- lib/presentation/screens/home/hadith/hadith_tab.dart
+++ lib/presentation/screens/home/hadith/hadith_tab.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islamy_training/core/assets_manger.dart';
+import 'package:islamy_training/presentation/screens/home/hadith/widgets/hadith_header_widget.dart';
 
-class HadithTab extends StatelessWidget {
-  const HadithTab({super.key});
+class HadithTab extends StatefulWidget {
+  HadithTab({super.key});
+
+  @override
+  State<HadithTab> createState() => _HadithTabState();
+}
+
+class _HadithTabState extends State<HadithTab> {
+  List<HadithItem> hadiths = [];
 
   @override
   Widget build(BuildContext context) {
+    if (hadiths.isEmpty) readHadith();
     return Scaffold(
       body: Column(
         children: [
-          Container(child: Image.asset(AssetsManger.hadithHeader)),
-          Divider(),
-          Text('الاحاديث',
-              style: Theme.of(context).textTheme.titleMedium,
-              textAlign: TextAlign.center),
-          Divider()
+          Expanded(flex: 2, child: Image.asset(AssetsManger.hadithHeader)),
+          const Divider(),
+          Text(
+            'الاحاديث',
+            style: Theme.of(context).textTheme.titleMedium,
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.rtl,
+          ),
+          const Divider(),
+          Expanded(
+            flex: 3,
+            child: ListView.separated(
+              separatorBuilder: (context, index) => Divider(),
+              itemBuilder: (context, index) =>
+                  HadithHeaderWidget(hadith: hadiths[index]),
+              itemCount: hadiths.length,
+            ),
+          )
         ],
       ),
     );
   }
+
+  void readHadith() async {
+    String ahadithContent =
+        await rootBundle.loadString('assets/files/ahadeth.txt');
+    List<String> ahadith = ahadithContent.split('#');
+    for (int i = 0; i < ahadith.length; i++) {
+      List<String> hadithLines = ahadith[i].trim().split('\n');
+      String hadithTitle = hadithLines[0];
+      String hadithContent = hadithLines.removeAt(0);
+      HadithItem hadith =
+          HadithItem(title: hadithTitle, content: hadithContent);
+      hadiths.add(hadith);
+    }
+    setState(() {});
+  }
+}
+
+class HadithItem {
+  String title;
+  String content;
+
+  HadithItem({required this.title, required this.content});
 }
---
+++ lib/presentation/screens/home/hadith/widgets/hadith_header_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:islamy_training/presentation/screens/home/hadith/hadith_tab.dart';
+
+class HadithHeaderWidget extends StatelessWidget {
+  HadithHeaderWidget({super.key, required this.hadith});
+
+  HadithItem hadith;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Text(
+        hadith.title,
+        style: Theme.of(context).textTheme.titleMedium,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.rtl,
+      ),
+    );
+  }
+}
