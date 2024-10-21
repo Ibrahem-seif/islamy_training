1cef834b4a5f812748d82223b3abed3a32e089d2
Ibrahem-seif <ibrahemsaeed118@gmail.com>
2024-10-22 00:53:33 +0300

    +

--- lib/core/assets_manger.dart
+++ lib/core/assets_manger.dart
@@ -6,4 +6,6 @@ class AssetsManger {
   static const String radioIcon = 'assets/images/radio_icon.png';
   static const String quranHeader = 'assets/images/quran_header.png';
   static const String hadithHeader = 'assets/images/hadith_header.png';
+  static const String sebhaHeader = 'assets/images/head_of_seb7a.png';
+  static const String sebhaBody = 'assets/images/body_of_seb7a.png';
 }
